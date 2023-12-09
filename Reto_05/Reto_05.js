6fe6f564beab4a1dc3fd54a86a236b3ad421415a
WillianAbrego <willianantonioabrego@gmail.com>
2023-12-09 16:21:57 -0600

    Reto 05

---
+++ Reto_05/Reto_05.js
@@ -0,0 +1,59 @@
+/*Santa 🎅 está probando su nuevo trineo eléctrico, el CyberReindeer, en una carretera del Polo Norte. La carretera se representa con una cadena de caracteres, donde:
+
+. = Carretera
+S = Trineo de Santa
+* = Barrera abierta
+| = Barrera cerrada
+Ejemplo de carretera: S...|....|.....
+
+Cada unidad de tiempo, el trineo avanza una posición a la derecha. Si encuentra una barrera cerrada, se detiene hasta que la barrera se abra. Si está abierta, la atraviesa directamente.
+
+Todas las barreras empiezan cerradas, pero después de 5 unidades de tiempo, se abren todas para siempre.
+
+Crea una función que simule el movimiento del trineo durante un tiempo dado y devuelva un array de cadenas representando el estado de la carretera en cada unidad de tiempo:
+*/
+const road = "S..|...|..";
+const time = 10; // unidades de tiempo
+const result = cyberReindeer(road, time);
+
+/* -> result:
+[
+  'S..|...|..', // estado inicial
+  '.S.|...|..', // avanza el trineo la carretera
+  '..S|...|..', // avanza el trineo la carretera
+  '..S|...|..', // el trineo para en la barrera
+  '..S|...|..', // el trineo para en la barrera
+  '...S...*..', // se abre la barrera, el trineo avanza
+  '...*S..*..', // avanza el trineo la carretera
+  '...*.S.*..', // avanza el trineo la carretera
+  '...*..S*..', // avanza el trineo la carretera
+  '...*...S..', // avanza por la barrera abierta
+]
+*/
+/*
+El resultado es un array donde cada elemento muestra la carretera en cada unidad de tiempo.
+
+Ten en cuenta que si el trineo está en la misma posición que una barrera, entonces toma su lugar en el array.
+
+Los elfos se inspiraron en este reto de Code Wars.
+*/
+
+function cyberReindeer(road, time) {
+  const roadChange = [];
+  let oldChar = ".";
+
+  for (let i = 0; i < time; i++) {
+    roadChange.push(road);
+
+    const sleighPos = road.indexOf("S");
+
+    if (i === 4) road = road.replace(/\|/g, "*");
+
+    if (road[sleighPos + 1] !== "|") {
+      road = road.slice(0, sleighPos) + oldChar + road.slice(sleighPos + 1);
+      oldChar = road[sleighPos + 1];
+      road = road.slice(0, sleighPos + 1) + "S" + road.slice(sleighPos + 2);
+    }
+  }
+  return roadChange;
+}
